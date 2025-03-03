710cfb0ce07f819d4eae319df6ffebd93863a12d
Ton Smeele <a.p.m.smeele@uu.nl>
2025-03-03 15:15:13 +0100

    refactor ChksumDataObject to use irods api object

--- src/main/ChksumDataObject.java
+++ src/main/ChksumDataObject.java
@@ -2,13 +2,14 @@ package main;
 
 import java.io.IOException;
 
-import nl.tsmeele.myrods.api.RcDataObjChksum;
-import nl.tsmeele.myrods.api.RcObjStat;
+import nl.tsmeele.myrods.api.Irods;
 import nl.tsmeele.myrods.apiDataStructures.DataObjInp;
 import nl.tsmeele.myrods.apiDataStructures.KeyValPair;
 import nl.tsmeele.myrods.apiDataStructures.Message;
 import nl.tsmeele.myrods.apiDataStructures.ObjType;
 import nl.tsmeele.myrods.apiDataStructures.RodsObjStat;
+import nl.tsmeele.myrods.irodsDataTypes.RcDataObjChksum;
+import nl.tsmeele.myrods.irodsDataTypes.RcObjStat;
 import nl.tsmeele.myrods.plumbing.ServerConnection;
 import nl.tsmeele.myrods.plumbing.MyRodsException;
 
@@ -18,48 +19,42 @@ import nl.tsmeele.myrods.plumbing.MyRodsException;
  *
  */
 public class ChksumDataObject {
-	private ServerConnection irodsSession;
+	private Irods irods;
 
-	public ChksumDataObject(ServerConnection irodsSession) {
-		this.irodsSession = irodsSession;
+	public ChksumDataObject(Irods irods) {
+		this.irods = irods;
 	}
 
 	public void execute(String objectPath) throws MyRodsException, IOException {
-		RcObjStat rcObjStat = new RcObjStat(objectPath, ObjType.DATAOBJECT);
-		Message reply = rcObjStat.sendTo(irodsSession);
-		if (reply.getIntInfo() < 0) {
+		RodsObjStat rodsObjStat = irods.rcObjStat(objectPath, ObjType.DATAOBJECT);
+		if (irods.error) {
 			// errorcode -31000 = DATA OBJECT DOES NOT EXIST
-			System.out.println("Data object " + objectPath + " does not exist, unable to continue");
+			System.out.println("Data object " + objectPath + " does not exist, unable to continue. error = " + irods.returnCode);
 			return;
 		} else {
-			System.out.println(reply.getMessage());
-			RodsObjStat stat = (RodsObjStat) reply.getMessage();
 			System.out.println("Status information on '" + objectPath + "' before chksum call");
-			System.out.println(stat);
+			System.out.println(rodsObjStat);
 		}
 
 		System.out.println("\nWe send a request to checksum the data object via a call to rcDataObjChksum");
 		KeyValPair kvChksum = new KeyValPair();
 		kvChksum.put("ChksumAll", "");
 		DataObjInp dataObjInp = new DataObjInp(objectPath, kvChksum);
-		RcDataObjChksum rcDataObjChksum = new RcDataObjChksum(dataObjInp);
-		reply = rcDataObjChksum.sendTo(irodsSession);
-		int info = reply.getIntInfo();
-		if (info >= 0) {
-			System.out.println("Chksum call reports success");
+		String checksum = irods.rcDataObjChksum(dataObjInp);
+		if (irods.error) {
+			System.out.println("Chksum call reports failure, ierror = " + irods.returnCode );
 		} else {
-			System.out.println("Chksum call reports failure, ierror = " + info );
+			System.out.println("Chksum call reports success. Checksum is: " + checksum);
 		}
 
 		System.out.println("\nStatus information on '" + objectPath + "' after chksum call");
-		reply = rcObjStat.sendTo(irodsSession);
-		if (reply.getIntInfo() < 0) {
+		rodsObjStat = irods.rcObjStat(objectPath, ObjType.DATAOBJECT);
+		if (irods.error) {
+			// errorcode -31000 = DATA OBJECT DOES NOT EXIST
 			System.out.println("Data object " + objectPath + " does not exist, unable to continue");
 			return;
 		} else {
-			RodsObjStat stat = (RodsObjStat) reply.getMessage();
-			System.out.println("Status information on '" + objectPath + "' after chksum call");
-			System.out.println(stat);
+			System.out.println(rodsObjStat);
 		}
 
 	}
