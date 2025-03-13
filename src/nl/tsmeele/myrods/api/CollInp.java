22e10637702ac1fd80da547f7cae2340c1b39e91
Ton Smeele <a.p.m.smeele@uu.nl>
2025-03-13 16:53:38 +0100

    Simplified constructor for CollInp

--- src/nl/tsmeele/myrods/api/CollInp.java
+++ src/nl/tsmeele/myrods/api/CollInp.java
@@ -5,6 +5,14 @@ import nl.tsmeele.myrods.irodsStructures.DataString;
 import nl.tsmeele.myrods.irodsStructures.DataStruct;
 
 public class CollInp extends DataStruct {
+	
+	/*
+	 * collName 				- name of the collection
+	 * flags  					- only used by rcOpenCollection
+	 * oprType 					- currently not used 
+	 * condInput 				- option, supports the following keywords:
+	 *  	RECURSIVE_OPR__KW	- create parent collections as needed. This keyword has no value. 
+	 */
 
 	public CollInp(String collName, int flags, int oprType, KeyValPair condInput) {
 		super("CollInpNew_PI");
@@ -12,6 +20,14 @@ public class CollInp extends DataStruct {
 		add(new DataInt("flags", flags));
 		add(new DataInt("oprType", oprType));
 		add(condInput);
+	}
+	
+	public CollInp(String collName) {
+		super("CollInpNew_PI");
+		add(new DataString("collName", collName));
+		add(new DataInt("flags", 0));
+		add(new DataInt("oprType", 0));
+		add(new KeyValPair());
 	}
 
 }
