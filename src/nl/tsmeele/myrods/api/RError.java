4b91294e223cdd9958d4cc172c22b8b72a1afda7
Ton Smeele <a.p.m.smeele@uu.nl>
2025-03-26 16:51:27 +0100

    fix PI processing of 'count' parm, add pointer parsing

--- src/nl/tsmeele/myrods/api/RError.java
+++ src/nl/tsmeele/myrods/api/RError.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import nl.tsmeele.myrods.irodsStructures.Data;
+import nl.tsmeele.myrods.irodsStructures.DataPtr;
 import nl.tsmeele.myrods.irodsStructures.DataStruct;
 
 // "RErrMsg_PI", "int status; str msg[ERR_MSG_LEN];",
@@ -17,9 +18,16 @@ public class RError extends DataStruct {
 	public RError(DataStruct dataStruct) {
 		super("RError_PI");
 		Iterator<Data> it = dataStruct.iterator();
+		if (!it.hasNext()) return;
+		// read count
+		it.next();
+		// read messages
 		while (it.hasNext()) {
-			RErrMsg rErrMsg = new RErrMsg((DataStruct) it.next());
-			errorMessages.add(rErrMsg);
+			DataPtr ptr = (DataPtr) it.next();
+			if (ptr.get(0) != null) { 
+				RErrMsg rErrMsg = new RErrMsg((DataStruct) ptr.get(0));
+				errorMessages.add(rErrMsg);
+			}
 		}
 	}
 
